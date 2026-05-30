e0765495853bef228bebe8112daaee55b22198bf
Spaide-LTD <wellmans.ai0@gmail.com>
2026-05-31 02:28:38 +0300

    Update main.js

--- Website/main.js
+++ Website/main.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  
   const reveals = document.querySelectorAll(".reveal");
 
   if ("IntersectionObserver" in window) {
